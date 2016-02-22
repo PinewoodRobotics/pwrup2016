f1c6f5eb88046e1174ec1212b77178a2013a0a60
Dean Reece <dean@reeceweb.com>
2016-02-21 17:22:04 -0800

    Final code with auton

--- 2016 Bench Test/src/org/usfirst/frc/team4765/robot/commands/Auton.java
+++ 2016 Bench Test/src/org/usfirst/frc/team4765/robot/commands/Auton.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team4765.robot.commands;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.WaitCommand;
 
 /**
  *
@@ -26,35 +27,37 @@ public class Auton extends CommandGroup
 		// e.g. if Command1 requires chassis, and Command2 requires arm,
 		// a CommandGroup containing them would require both the chassis and the
 		// arm.
+		addSequential(new ResetFourBar());
 		switch (setting)
 		{
-			case 0:
-				System.out.println("0");
-				break;
-			
 			case 1:
-				// Move until wall is hit
-				System.out.println("case 1");
-				addSequential(new Init());
-				addSequential(new ResetFourBar());
-				addSequential(new MoveForwardTime(2.0));
+				System.out.println("case 1 DO NOTHING");
 				break;
 			
 			case 2:
-				System.out.println("case 2");
-				//addSequential(new MoveForwardTime(2.0));
+				// Move until wall is hit
+				System.out.println("case 2 MOVE A BIT");
+				addSequential(new MoveForwardThrottleTime(0.1, 0.5));
 				break;
 			
-			case 3:
-				System.out.println("case 3");
+			case 3: 
+				//RAMP ART, MOAT
+				System.out.println("case 3 RAMP ART, MOAT");
+				addSequential(new MoveForwardThrottleTime(0.3937, 3.0));
 				break;
-				
+			
 			case 4:
-				System.out.println("case 4");
+				// ROCK WALL
+				System.out.println("case 4 ROCK WALL");
+				addSequential(new MoveForwardThrottleTime(0.55, 3.0));
 				break;
 				
 			case 5:
-				System.out.println("case 5");
+				// LOW BAR
+				System.out.println("case 5 LOW BAR");
+				addSequential(new FourBarToLowBar());
+				addSequential(new WaitCommand(2));
+				addSequential(new MoveForwardThrottleTime(0.102, 3.0));
 				break;
 			
 			default:
